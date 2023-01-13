9eb8f5f1a93007c7603a8398c355cbd54afae86d
asmaaelhadad <asmaaelhadad@gmail.com>
2023-01-13 14:00:40 +0100

    before breack

--- src/viking.js
+++ src/viking.js
@@ -1,9 +1,30 @@
 // Soldier
-class Soldier {}
+class Soldier {
+
+    constructor(health , strength ){ 
+        this.health =  health
+        this.strength = strength
+       }
+   attack() { return this.strength ;
+     } 
+   receiveDamage(damage) {
+    this.health -= damage ;
+}
+}
 
 // Viking
-class Viking {}
-
+class Viking extends Soldier {
+contructor(name; health , strength){
+    super(name , health , strength )
+    this.name = name
+}
+receiveDamage(damage) {
+    this.health -= damage ;
+}
+battleCry(damage) {
+    this.health -= damage ;
+}
+}
 // Saxon
 class Saxon {}
 
